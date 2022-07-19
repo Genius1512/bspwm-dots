0116ce47be1a759a1d115f6de1d430e681afe299
Genius1512 <silvan.genius@gmail.com>
2022-07-19 17:15:06 +0200

    LVim config

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,10 @@ sudo ./install_lvim.sh -y
 ./install_lvim.sh -y
 echo "export PATH='$PATH:~/.local/bin'" >> ~/.bashrc
 
+# Get LunarVim config
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/LunarVim/config/config.lua -O ~/.config/lvim/config.lua
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/LunarVim/config/plugins.lua -O ~/.local/share/lunarvim/lvim/lua/lvim/plugins.lua
+https://raw.githubusercontent.com/Genius1512/dotfiles/main/LunarVim/config/keymappings.lua -O ~/local/share/lunarvim/lvim/lua/lvim/keymappings.lua
+
 source ~/.bashrc
 
