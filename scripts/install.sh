67d18f991ec559a944dc85bb1f9ad37589e1d26e
Genius1512 <silvan.genius@gmail.com>
2022-07-19 16:03:24 +0200

    Run LunarVim Installer as sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@
 # Install LunarVim
 wget https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install.sh -O install_lvim.sh
 chmod +x install_lvim.sh
-./install_lvim.sh -y
+sudo ./install_lvim.sh -y
 echo "export PATH='$PATH:~/.local/bin'" >> ~/.bashrc
 
