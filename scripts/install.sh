57a97f977b3564fca9fcec7fb97825535177de63
Genius1512 <silvan.genius@gmail.com>
2022-07-19 11:28:18 +0200

    Add local/bin to PATH

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,9 +23,16 @@ wget https://sh.rustup.rs
 mv index.html install_rust.sh
 chmod +x install_rust.sh
 ./install_rust.sh -y
+source /home/silva/.cargo/env
 
 # Install LunarVim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 sudo apt install -y ./nvim-linux64.deb
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+
+sudo chmod +rwx /home/silva/.local
+
+echo "export PATH=$PATH:/home/silva/.local/bin"
+
+source /home/silva/.bashrc
 
