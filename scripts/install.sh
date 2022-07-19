ac828cb527e4ea0179f358bf5bfeaf136799fe9b
Genius1512 <silvan.genius@gmail.com>
2022-07-19 12:57:13 +0200

    Editing install_lvim

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,19 +15,20 @@ wget https://sh.rustup.rs
 mv index.html install_rust.sh
 chmod +x install_rust.sh
 ./install_rust.sh -y
-export PATH="$PATH:/home/silva/.cargo/bin"
+source /home/silva/.cargo/env
 
 # Install LunarVim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 sudo apt install -y ./nvim-linux64.deb
 wget https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh
 mv install.sh install_lvim.sh
+sed -i "s/command -v cargo/command -v \/home\/silva\/.cargo\/bin\/cargo/" install_lvim.sh
 chmod +x install_lvim.sh
 sudo ./install_lvim.sh -y
 
 sudo chmod +rwx /home/silva/.local
 
-echo "export PATH=$PATH:/home/silva/.local/bin"
+echo "export PATH=$PATH:/home/silva/.local/bin" >> /home/silva/.bashrc
 
 source /home/silva/.bashrc
 
