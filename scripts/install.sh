a0e639c59fbb7da0e5e0e40fabe9ea73549acd0f
Genius1512 <silvan.genius@gmail.com>
2022-07-19 10:01:53 +0200

    LunarVim

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,24 +13,19 @@ cd /tmp
 sudo apt install -y curl
 sudo apt install -y wget
 sudo apt install -y git
-
-# Install neovim
-wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb -y
-
-# Install kde
-# sudo apt install kde-plasma-desktop
-
-# Snaps
-sudo apt install -y snapd
-
-snap install firefox
+sudo apt install -y npm
+sudo apt install -y pip
 
 # Install rust
 wget https://sh.rustup.rs
 mv index.html install_rust.sh
 chmod +x install_rust.sh
 ./install_rust.sh -y
+
+# Install LunarVim
+wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+sudo apt install -y ./nvim-linux64.deb
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 
 # Reboot
 sudo reboot
