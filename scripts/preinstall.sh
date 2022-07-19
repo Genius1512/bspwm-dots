afbc7952426ee6a62878aa44909017174f7cf6e7
Genius1512 <silvan.genius@gmail.com>
2022-07-19 14:37:48 +0200

    Install rust first version

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+cd /tmp
+
+# Utils
+sudo apt install wget
+
+# Install the rust toolchain
+wget sh.rustup.rs
+mv index.html install_rust.sh
+chmod +x install_rust.sh
+./install_rust.sh -y --default-toolchain stable
+
