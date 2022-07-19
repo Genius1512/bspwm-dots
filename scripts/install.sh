bdfe4ad8b5e71ca5604ba5a3f3b6a737ce4b96d2
Genius1512 <silvan.genius@gmail.com>
2022-07-19 13:44:15 +0200

    Try fix n. 5

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,6 +17,7 @@ chmod +x install_rust.sh
 ./install_rust.sh -y
 source /home/silva/.cargo/env
 /home/silva/.cargo/bin/rustup default stable
+PATH="$PATH:/home/silva/.cargo/bin"
 
 # Install LunarVim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
@@ -24,7 +25,6 @@ sudo apt install -y ./nvim-linux64.deb
 wget https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh
 mv install.sh install_lvim.sh
 sed -i "s/command -v cargo/command -v \/home\/silva\/.cargo\/bin\/cargo/" install_lvim.sh
-sed -i "s/cargo install/\/home\/silva\/.cargo\/bin\/cargo install -v/" install_lvim.sh
 chmod +x install_lvim.sh
 sudo ./install_lvim.sh -y
 
