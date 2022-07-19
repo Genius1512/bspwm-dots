dd032a76deeb737384f77914822867f9e3d60434
Genius1512 <silvan.genius@gmail.com>
2022-07-19 14:00:03 +0200

    Revert

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,17 +16,15 @@ mv index.html install_rust.sh
 chmod +x install_rust.sh
 ./install_rust.sh -y
 source /home/silva/.cargo/env
+/home/silva/.cargo/bin/rustup default stable
 PATH="$PATH:/home/silva/.cargo/bin"
-rustup default stable
-cargo --version
-
-read -p "Rust installed" </dev/tty
 
 # Install LunarVim
 wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 sudo apt install -y ./nvim-linux64.deb
 wget https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh
 mv install.sh install_lvim.sh
+sed -i "s/command -v cargo/command -v \/home\/silva\/.cargo\/bin\/cargo/" install_lvim.sh
 chmod +x install_lvim.sh
 sudo ./install_lvim.sh -y
 
