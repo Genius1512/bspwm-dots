b2b9082348df6b07e8d17fbb6f48c8f023a69122
Genius1512 <silvan.genius@gmail.com>
2022-07-19 17:16:44 +0200

    Remove duplicated install_lvim

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@
 # Install LunarVim
 wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/LunarVim/install.sh -O install_lvim.sh
 chmod +x install_lvim.sh
-sudo ./install_lvim.sh -y
 ./install_lvim.sh -y
 echo "export PATH='$PATH:~/.local/bin'" >> ~/.bashrc
 
