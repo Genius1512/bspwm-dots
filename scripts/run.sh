1e6ed787ff20e1dce8b84eac81fa3795a903f12f
Genius1512 <silvan.genius@gmail.com>
2022-07-19 15:37:54 +0200

    Fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
-wget https://github.com/Genius1512/dotfiles/blob/main/scripts/preinstall.sh
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/scripts/preinstall.sh
 source preinstall.sh
 
-wget https://github.com/Genius1512/dotfiles/blob/main/scripts/install.sh
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/scripts/install.sh
 source install.sh
 
-wget https://github.com/Genius1512/dotfiles/blob/main/scripts/postinstall.sh
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/scripts/postinstall.sh
 source postinstall.sh
 
 rm preinstall.sh
