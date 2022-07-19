d54f224e381c1b7d0d8fb5078e26a7ed1e60dafe
Silvan Schmidt <silvan.genius@gmail.com>
2022-07-19 16:45:56 +0200

    Removed sudo check

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install LunarVim
-wget https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install.sh -O install_lvim.sh
+wget https://raw.githubusercontent.com/Genius1512/dotfiles/main/LunarVim/install.sh -O install_lvim.sh
 chmod +x install_lvim.sh
 sudo ./install_lvim.sh -y
 ./install_lvim.sh -y
