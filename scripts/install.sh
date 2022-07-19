3bd77a6b3dca9ff2e5230bb945cb9875d725d04e
Genius1512 <silvan.genius@gmail.com>
2022-07-19 15:28:11 +0200

    Install Lvim and postinstall.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# Install LunarVim
+wget https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install.sh -O install_lvim.sh
+chmod +x install_lvim.sh
+./install_lvim.sh -y
+echo "export PATH='$PATH:~/.local/bin'" >> ~/.bashrc
+
---
+++ scripts/postinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm install_rust.sh
+rm install_lvim.sh
+
+source ~/.bashrc
